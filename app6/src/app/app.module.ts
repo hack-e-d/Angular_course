e04eda9910a46645fec5d8141628e1011a7321d8
Vijay P V <vijayraj1998@yahoo.com>
2020-10-29 11:29:57 +0530

    Add files via upload

---
+++ app6/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
